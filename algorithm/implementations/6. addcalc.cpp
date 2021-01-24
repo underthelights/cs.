6b3af3cb51223de325b1f3bc07bfc59ded7f8c3f
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-01-24 20:48:50 +0900

    Create 6. addcalc.cpp

---
+++ algorithm/implementations/6. addcalc.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+#define O ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define LL long long
+
+int main(){
+    cout << "input first num : ";
+    int a=0, b=0;
+    cin>> a;
+    cout << "input second num : ";
+    cin>> b;
+
+    cout << a << " + " << b << " = " << a+b << endl;
+    return 0;
+}
