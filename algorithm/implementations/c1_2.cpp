3089e5ba80d97083bc4a52b2971d74b2fa71bc4b
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-01-30 16:44:31 +0900

    Create c1_2.cpp

---
+++ algorithm/implementations/c1_2.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+#define O ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define LL long long
+
+int main(){
+    int a, b;
+
+    cin>>a>>b;
+    swap(a,b);
+
+    cout<<a << " " << b<<"\n";
+    return 0;
+}
+
